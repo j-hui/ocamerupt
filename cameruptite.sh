0292f122f4f8066de17d68dc976aafd61f5c6cc6
John Hui <jwongsquared@gmail.com>
2018-09-20 17:59:52 -0400

    added cameruptite

---
+++ cameruptite.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+set -eu
+
+#LLVM_VER=-5.0
+
+UTILS=python3 clang tree htop git svn vim emacs
+OPAM_DEPS=gcc make python2.7 m4 pkg-config unzip cmake
+OCAML_PKGS=ocaml opam
+OPAM_PKGS=
+
+update_cmd() { apt update -y && apt upgrade -y }
+install_cmd() { apt install -y "$@" }
+
+
+report() { echo " = = = => [CAMERUPTITE]:" "$@" }
+log_do() {
+    local name=$1
+    shift
+    echo "========"
+    report "Starting:" "${name}" "..."
+    "$@"
+    report "Complete:" "${name}"
+    echo "========"
+}
+
+cd ~
+
+log_do "update package manager" update_cmd
+
+log_do "install some handy utilities" install_cmd ${UTILS} 
+
+log_do "install OPAM, and dependencies" install_cmd ${OPAM_DEPS} ${OCAML_PKGS}
+
+log_do "installing LLVM" install_cmd "llvm${LLVM_VER}"
+
+ll_ver="$(llvm-config --version | sed 's/\(3\.[[:digit:]]\)\.0/\1/')"
+
+report "installed LLVM version ${ll_ver}"
+
+log_do "OPAM init" opam init --auto-setup
+
+log_do "OPAM env" eval `opam config env`
+
+log_do "install LLVM bindings (${ll_ver})" opam install -y llvm.${ll_ver}
+
+report "mega evolution complete!"
