550241d60f174558c343f034c1da3e55571e0696
John Hui <jwongsquared@gmail.com>
2018-09-20 18:39:18 -0400

    actual fix for shell file

--- cameruptite.sh
+++ cameruptite.sh
@@ -1,141 +1,47 @@
-0000000    U   s   a   g   e   :       d   o   s   2   u   n   i   x    
-0000020    [   o   p   t   i   o   n   s   ]       [   f   i   l   e    
-0000040    .   .   .   ]       [   -   n       i   n   f   i   l   e    
-0000060    o   u   t   f   i   l   e       .   .   .   ]  \n       -   -
-0000100    a   l   l   o   w   -   c   h   o   w   n                    
-0000120                    a   l   l   o   w       f   i   l   e       o
-0000140    w   n   e   r   s   h   i   p       c   h   a   n   g   e  \n
-0000160        -   a   s   c   i   i                                    
-0000200                                c   o   n   v   e   r   t       o
-0000220    n   l   y       l   i   n   e       b   r   e   a   k   s    
-0000240    (   d   e   f   a   u   l   t   )  \n       -   i   s   o    
-0000260                                                                 
-0000300        c   o   n   v   e   r   s   i   o   n       b   e   t   w
-0000320    e   e   n       D   O   S       a   n   d       I   S   O   -
-0000340    8   8   5   9   -   1       c   h   a   r   a   c   t   e   r
-0000360        s   e   t  \n               -   1   2   5   2            
-0000400                                                    u   s   e    
-0000420    W   i   n   d   o   w   s       c   o   d   e       p   a   g
-0000440    e       1   2   5   2       (   W   e   s   t   e   r   n    
-0000460    E   u   r   o   p   e   a   n   )  \n               -   4   3
-0000500    7                                                            
-0000520        u   s   e       D   O   S       c   o   d   e       p   a
-0000540    g   e       4   3   7       (   U   S   )       (   d   e   f
-0000560    a   u   l   t   )  \n               -   8   5   0            
-0000600                                                        u   s   e
-0000620        D   O   S       c   o   d   e       p   a   g   e       8
-0000640    5   0       (   W   e   s   t   e   r   n       E   u   r   o
-0000660    p   e   a   n   )  \n               -   8   6   0            
-0000700                                                        u   s   e
-0000720        D   O   S       c   o   d   e       p   a   g   e       8
-0000740    6   0       (   P   o   r   t   u   g   u   e   s   e   )  \n
-0000760                -   8   6   3                                    
-0001000                                u   s   e       D   O   S       c
-0001020    o   d   e       p   a   g   e       8   6   3       (   F   r
-0001040    e   n   c   h       C   a   n   a   d   i   a   n   )  \n    
-0001060            -   8   6   5                                        
-0001100                            u   s   e       D   O   S       c   o
-0001120    d   e       p   a   g   e       8   6   5       (   N   o   r
-0001140    d   i   c   )  \n       -   7                                
-0001160                                                    c   o   n   v
-0001200    e   r   t       8       b   i   t       c   h   a   r   a   c
-0001220    t   e   r   s       t   o       7       b   i   t       s   p
-0001240    a   c   e  \n       -   b   ,       -   -   k   e   e   p   -
-0001260    b   o   m                                   k   e   e   p    
-0001300    B   y   t   e       O   r   d   e   r       M   a   r   k  \n
-0001320        -   c   ,       -   -   c   o   n   v   m   o   d   e    
-0001340                                c   o   n   v   e   r   s   i   o
-0001360    n       m   o   d   e  \n               c   o   n   v   m   o
-0001400    d   e                                                   a   s
-0001420    c   i   i   ,       7   b   i   t   ,       i   s   o   ,    
-0001440    m   a   c   ,       d   e   f   a   u   l   t       t   o    
-0001460    a   s   c   i   i  \n       -   f   ,       -   -   f   o   r
-0001500    c   e                                               f   o   r
-0001520    c   e       c   o   n   v   e   r   s   i   o   n       o   f
-0001540        b   i   n   a   r   y       f   i   l   e   s  \n       -
-0001560    h   ,       -   -   h   e   l   p                            
-0001600                        d   i   s   p   l   a   y       t   h   i
-0001620    s       h   e   l   p       t   e   x   t  \n       -   i   ,
-0001640        -   -   i   n   f   o   [   =   F   L   A   G   S   ]    
-0001660                d   i   s   p   l   a   y       f   i   l   e    
-0001700    i   n   f   o   r   m   a   t   i   o   n  \n               f
-0001720    i   l   e       .   .   .                                    
-0001740                f   i   l   e   s       t   o       a   n   a   l
-0001760    y   z   e  \n       -   k   ,       -   -   k   e   e   p   d
-0002000    a   t   e                                   k   e   e   p    
-0002020    o   u   t   p   u   t       f   i   l   e       d   a   t   e
-0002040   \n       -   L   ,       -   -   l   i   c   e   n   s   e    
-0002060                                    d   i   s   p   l   a   y    
-0002100    s   o   f   t   w   a   r   e       l   i   c   e   n   s   e
-0002120   \n       -   l   ,       -   -   n   e   w   l   i   n   e    
-0002140                                    a   d   d       a   d   d   i
-0002160    t   i   o   n   a   l       n   e   w   l   i   n   e  \n    
-0002200    -   m   ,       -   -   a   d   d   -   b   o   m            
-0002220                            a   d   d       B   y   t   e       O
-0002240    r   d   e   r       M   a   r   k       (   d   e   f   a   u
-0002260    l   t       U   T   F   -   8   )  \n       -   n   ,       -
-0002300    -   n   e   w   f   i   l   e                                
-0002320        w   r   i   t   e       t   o       n   e   w       f   i
-0002340    l   e  \n               i   n   f   i   l   e                
-0002360                                            o   r   i   g   i   n
-0002400    a   l       f   i   l   e       i   n       n   e   w   -   f
-0002420    i   l   e       m   o   d   e  \n               o   u   t   f
-0002440    i   l   e                                                    
-0002460    o   u   t   p   u   t       f   i   l   e       i   n       n
-0002500    e   w   -   f   i   l   e       m   o   d   e  \n       -   -
-0002520    n   o   -   a   l   l   o   w   -   c   h   o   w   n        
-0002540                    d   o   n   '   t       a   l   l   o   w    
-0002560    f   i   l   e       o   w   n   e   r   s   h   i   p       c
-0002600    h   a   n   g   e       (   d   e   f   a   u   l   t   )  \n
-0002620        -   o   ,       -   -   o   l   d   f   i   l   e        
-0002640                                w   r   i   t   e       t   o    
-0002660    o   l   d       f   i   l   e       (   d   e   f   a   u   l
-0002700    t   )  \n               f   i   l   e       .   .   .        
-0002720                                            f   i   l   e   s    
-0002740    t   o       c   o   n   v   e   r   t       i   n       o   l
-0002760    d   -   f   i   l   e       m   o   d   e  \n       -   q   ,
-0003000        -   -   q   u   i   e   t                                
-0003020                q   u   i   e   t       m   o   d   e   ,       s
-0003040    u   p   p   r   e   s   s       a   l   l       w   a   r   n
-0003060    i   n   g   s  \n       -   r   ,       -   -   r   e   m   o
-0003100    v   e   -   b   o   m                           r   e   m   o
-0003120    v   e       B   y   t   e       O   r   d   e   r       M   a
-0003140    r   k       (   d   e   f   a   u   l   t   )  \n       -   s
-0003160    ,       -   -   s   a   f   e                                
-0003200                    s   k   i   p       b   i   n   a   r   y    
-0003220    f   i   l   e   s       (   d   e   f   a   u   l   t   )  \n
-0003240        -   u   ,           -   -   k   e   e   p   -   u   t   f
-0003260    1   6                       k   e   e   p       U   T   F   -
-0003300    1   6       e   n   c   o   d   i   n   g  \n       -   u   l
-0003320    ,       -   -   a   s   s   u   m   e   -   u   t   f   1   6
-0003340    l   e       a   s   s   u   m   e       t   h   a   t       t
-0003360    h   e       i   n   p   u   t       f   o   r   m   a   t    
-0003400    i   s       U   T   F   -   1   6   L   E  \n       -   u   b
-0003420    ,       -   -   a   s   s   u   m   e   -   u   t   f   1   6
-0003440    b   e       a   s   s   u   m   e       t   h   a   t       t
-0003460    h   e       i   n   p   u   t       f   o   r   m   a   t    
-0003500    i   s       U   T   F   -   1   6   B   E  \n       -   v   ,
-0003520            -   -   v   e   r   b   o   s   e                    
-0003540                v   e   r   b   o   s   e       o   p   e   r   a
-0003560    t   i   o   n  \n       -   F   ,       -   -   f   o   l   l
-0003600    o   w   -   s   y   m   l   i   n   k           f   o   l   l
-0003620    o   w       s   y   m   b   o   l   i   c       l   i   n   k
-0003640    s       a   n   d       c   o   n   v   e   r   t       t   h
-0003660    e       t   a   r   g   e   t   s  \n       -   R   ,       -
-0003700    -   r   e   p   l   a   c   e   -   s   y   m   l   i   n   k
-0003720        r   e   p   l   a   c   e       s   y   m   b   o   l   i
-0003740    c       l   i   n   k   s       w   i   t   h       c   o   n
-0003760    v   e   r   t   e   d       f   i   l   e   s  \n            
-0004000                                                                 
-0004020                            (   o   r   i   g   i   n   a   l    
-0004040    t   a   r   g   e   t       f   i   l   e   s       r   e   m
-0004060    a   i   n       u   n   c   h   a   n   g   e   d   )  \n    
-0004100    -   S   ,       -   -   s   k   i   p   -   s   y   m   l   i
-0004120    n   k                   k   e   e   p       s   y   m   b   o
-0004140    l   i   c       l   i   n   k   s       a   n   d       t   a
-0004160    r   g   e   t   s       u   n   c   h   a   n   g   e   d    
-0004200    (   d   e   f   a   u   l   t   )  \n       -   V   ,       -
-0004220    -   v   e   r   s   i   o   n                                
-0004240        d   i   s   p   l   a   y       v   e   r   s   i   o   n
-0004260        n   u   m   b   e   r  \n                                
-0004270
+#!/bin/bash 
+
+set -eu
+
+#LLVM_VER=-5.0
+
+UTILS=python3 clang tree htop git svn vim emacs
+OPAM_DEPS=gcc make python2.7 m4 pkg-config unzip cmake
+OCAML_PKGS=ocaml opam
+OPAM_PKGS=
+
+update_cmd() { apt update -y && apt upgrade -y }
+install_cmd() { apt install -y "$@" }
+
+
+report() { echo " = = = => [CAMERUPTITE]:" "$@" }
+log_do() {
+    local name=$1
+    shift
+    echo "--------"
+    report "Starting:" "${name}" "..."
+    "$@"
+    report "Complete:" "${name}"
+    echo "--------"
+}
+
+cd ~
+
+log_do "update package manager" update_cmd
+
+log_do "install some handy utilities" install_cmd ${UTILS} 
+
+log_do "install OPAM, and dependencies" install_cmd ${OPAM_DEPS} ${OCAML_PKGS}
+
+log_do "installing LLVM" install_cmd "llvm${LLVM_VER}"
+
+ll_ver="$(llvm-config --version | sed 's/\(3\.[[:digit:]]\)\.0/\1/')"
+
+report "installed LLVM version ${ll_ver}"
+
+log_do "OPAM init" opam init --auto-setup
+
+log_do "OPAM env" eval `opam config env`
+
+log_do "install LLVM bindings (${ll_ver})" opam install -y llvm.${ll_ver}
+
+report "mega evolution complete!"
