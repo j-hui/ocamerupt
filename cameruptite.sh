10ed8738159102e3e1101dcdd61b6c38cb3cabba
John Hui <jwongsquared@gmail.com>
2018-09-21 00:29:19 -0400

    beefed up cameruptite

--- cameruptite.sh
+++ cameruptite.sh
@@ -1,11 +1,10 @@
 #!/bin/bash 
 
-set -eu
-
 #### BEGIN: script configs
 #           modify these as needed
 
-#LLVM_VER=-5.0
+# This will be overridden to @6 if on macOS
+#LLVM_VER=-6.0
 LLVM_VER=
 
 UTILS="python3 clang tree htop git subversion vim emacs"
@@ -13,11 +12,10 @@ OPAM_DEPS="gcc make python2.7 m4 pkg-config unzip cmake"
 OCAML_PKGS="ocaml opam"
 OPAM_PKGS=
 
-update_cmd() { sudo apt update -y && sudo apt upgrade -y ; }
-install_cmd() { sudo apt install -y "$@" ; }
 
 #### END: script configs
 
+#### BEGIN: helper functions and initial setup
 
 report() { echo " = = = => [CAMERUPTITE]:" "$@" ; }
 log_do() {
@@ -30,10 +28,43 @@ log_do() {
     echo "--------"
 }
 
-if [ "$(id -u)" == "0" ]; then
-    report "$0 should not be run as root. It already contains sudo where needed."
+if [[ "$(id -u)" == "0" ]]; then
+    report "$0 should not be run as root. It already contains sudo."
     exit 1
 fi
+
+case $(uname) in
+    "Linux")
+        case $(lsb_release -is) in
+            "Debian"|"Ubuntu")
+                update_cmd() { sudo apt update -y && sudo apt upgrade -y ; }
+                install_cmd() { sudo apt install -y "$@" ; }
+                ;;
+            *)
+                echo "Unsupported Linux distro, bailing now."
+                exit 1
+                ;;
+        esac
+        ;;
+    "Darwin")
+        if [[ ! hash "$1" 2> /dev/null ]]; then
+            report "Installing Homebrew, a macOS package manager..."
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+        update_cmd() { brew update && brew upgrade ; }
+        install_cmd() { brew install "$@" ; }
+        ;;
+    *)
+        echo "Unsupported operating system, bailing now."
+        exit 1
+        ;;
+esac
+
+#### END: helper functions and initial setup
+
+#### BEGIN: actual script
+
+set -eu
 
 cd ~
 
@@ -45,19 +76,22 @@ log_do "install OPAM, and dependencies" install_cmd ${OPAM_DEPS} ${OCAML_PKGS}
 
 log_do "installing LLVM" install_cmd "llvm${LLVM_VER}"
 
-ll_ver="$(llvm-config --version | sed 's/\(3\.[[:digit:]]\)\.0/\1/')"
+ll_ver="$(llvm-config | sed 's/\..$/.0/' | sed 's/\(3\.[[:digit:]]\)\.0/\1/')"
 
 report "installed LLVM version ${ll_ver}"
 
-log_do "OPAM init" opam init --auto-setup
-
-log_do "OPAM env" eval `opam config env`
+if ! opam list &> /dev/null; then
+    report "OPAM not initialized"
+    log_do "OPAM init" opam init --auto-setup
+    log_do "OPAM env" eval `opam config env`
+fi
 
 log_do "install LLVM bindings (${ll_ver})" opam install -y llvm.${ll_ver}
-
 
 if [[ ! -z "${OPAM_PKGS}" ]]; then
     log_do "install other OPAM packages" opam install -y ${OPAM_PKGS}
 fi
 
-report "mega evolution complete!"
+report "Mega evolution complete!"
+
+#### END: actual script
